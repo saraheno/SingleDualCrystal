144d1f6b6b8cf1be2d4f1d2524797d2e14409432
Sarah <sarah.eno@cern.ch>
2022-09-19 16:43:47 -0400

    fix

--- compact/condor-executable.sh
+++ compact/condor-executable.sh
@@ -9,14 +9,15 @@ echo "started at $START_TIME"
 # setup software environment at UMD
 #
 source /cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos7-gcc11-opt/setup.sh
-source ../../../bin/thisdd4hep.sh
-
+echo "ran setup"
+source  /data/users/eno/CalVision/dd4hep/DD4hep/bin/thisdd4hep.sh
+echo "ran thisdd4hep"
 #
 # run 
 #
 
 
-ddsim --steeringFile SCEPCALsteering.py --compact ./DRSingleCrystal.xml --runType batch --part.userParticleHandler='' -G --gun.position="0.,10.,0." --gun.direction "0 -1 0" --gun.energy "1*GeV" --gun.particle="mu-" --gun.distribution=uniform -N 1 -O out.root >& haha.log
+ddsim --steeringFile /home/eno/CalVision/dd4hep/DD4hep/examples/SingleDualCrystal/compact/SCEPCALsteering.py --compact /home/eno/CalVision/dd4hep/DD4hep/examples/SingleDualCrystal/compact/DRSingleCrystal.xml --runType batch --part.userParticleHandler='' -G --gun.position="0.,10.,0." --gun.direction "0 -1 0" --gun.energy "1*GeV" --gun.particle="mu-" --gun.distribution=uniform -N 1 -O out.root >& haha.log
 
 exitcode=$?
 
