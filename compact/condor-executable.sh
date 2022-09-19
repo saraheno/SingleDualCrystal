3100659bd89b0322822826097c620e54288c0f56
Sarah <sarah.eno@cern.ch>
2022-09-19 16:34:00 -0400

    add batch

---
+++ compact/condor-executable.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd /data/users/eno/CalVision/dd4hep/DD4hep/examples/SingleDualCrystal/compact/
+
+START_TIME=`/bin/date`
+echo "started at $START_TIME"
+
+
+# setup software environment at UMD
+#
+source /cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos7-gcc11-opt/setup.sh
+source ../../../bin/thisdd4hep.sh
+
+#
+# run 
+#
+
+
+ddsim --steeringFile SCEPCALsteering.py --compact ./DRSingleCrystal.xml --runType batch --part.userParticleHandler='' -G --gun.position="0.,10.,0." --gun.direction "0 -1 0" --gun.energy "1*GeV" --gun.particle="mu-" --gun.distribution=uniform -N 1 -O out.root >& haha.log
+
+exitcode=$?
+
+
+#
+# end run
+#
+
+echo ""
+END_TIME=`/bin/date`
+echo "finished at $END_TIME"
+exit $exitcode
