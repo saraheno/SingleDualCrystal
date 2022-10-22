b366daca03699f9d7b11ecf9faeeedbca086ec22
Sarah <sarah.eno@cern.ch>
2022-10-22 16:44:10 -0400

    ha

--- compact/junkst.py
+++ compact/junkst.py
@@ -143,7 +143,7 @@ SIM.field.stepper = "ClassicalRK4"
 
 
 ##  list of filter objects: map between name and parameter dictionary 
-SIM.filter.filters = {'geantino': {'name': 'GeantinoRejectFilter/GeantinoRejector', 'parameter': {}}, 'edep1kev': {'name': 'EnergyDepositMinimumCut', 'parameter': {'Cut': 0.001}}, 'edep0': {'name': 'EnergyDepositMinimumCut/Cut0', 'parameter': {'Cut': 0.0}}, 'wvmin900nm': {'name': 'WavelengthMinimumCut', 'parameter': {'Cut': 900.}},
+SIM.filter.filters = {'geantino': {'name': 'GeantinoRejectFilter/GeantinoRejector', 'parameter': {}}, 'edep1kev': {'name': 'EnergyDepositMinimumCut', 'parameter': {'Cut': 0.001}}, 'edep0': {'name': 'EnergyDepositMinimumCut/Cut0', 'parameter': {'Cut': 0.0}}, 'wvnm': {'name': 'WavelengthMinimumCut', 'parameter': {'Cut': 10.}}
 'wvwind': {'name': 'WavelengthnmwindCut', 'parameter': {'Cut': 990.}}
 }
 
@@ -152,8 +152,7 @@ SIM.filter.filters = {}
 
 
 
-SIM.filter.calo = "wvwind"
-
+SIM.filter.calo = "wvnm"
 
 
 ##  a map between patterns and filter objects, using patterns to attach filters to sensitive detector 
