9ab4edff8414114a710b6e249ecae779a13aff19
Sarah <sarah.eno@cern.ch>
2023-01-06 16:49:23 -0500

    dont store contrive for opt phot

--- src/DualCrystalCalorimeterSDAction.cpp
+++ src/DualCrystalCalorimeterSDAction.cpp
@@ -244,13 +244,13 @@ namespace dd4hep {
       //if(SCEPRINT) std::cout<<"NOT optical photon"<<std::endl;
 
         hit->energyDeposit += contrib.deposit;
-
+      hit->truth.emplace_back(contrib);
 
 
         //return true;
       }
 
-      hit->truth.emplace_back(contrib);
+
       mark(h.track);	
       return true;
 
