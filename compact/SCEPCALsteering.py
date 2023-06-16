6b84bd738f0911d06280c3388c200a92e039f8b2
Sarah <sarah.eno@cern.ch>
2023-06-16 12:47:01 -0400

    new output

--- compact/SCEPCALsteering.py
+++ compact/SCEPCALsteering.py
@@ -329,29 +329,22 @@ SIM.output.random = 3
 ## 
 ##     For example one can add this to the ddsim steering file:
 ## 
-##       def exampleUserPlugin(dd4hepSimulation):
-##         '''Example code for user created plugin.
-## 
-##         :param DD4hepSimulation dd4hepSimulation: The DD4hepSimulation instance, so all parameters can be accessed
-##         :return: None
-##         '''
-##         from DDG4 import EventAction, Kernel
-##         dd = dd4hepSimulation  # just shorter variable name
-##         evt_root = EventAction(Kernel(), 'Geant4Output2ROOT/' + dd.outputFile, True)
-##         evt_root.HandleMCTruth = True or False
-##         evt_root.Control = True
-##         if not dd.outputFile.endswith(dd.outputConfig.myExtension):
-##           output = dd.outputFile + dd.outputConfig.myExtension
-##         evt_root.Output = output
-##         evt_root.enableUI()
-##         Kernel().eventAction().add(evt_root)
-##         return None
-## 
-##       SIM.outputConfig.userOutputPlugin = exampleUserPlugin
-##       # arbitrary options can be created and set via the steering file or command line
-##       SIM.outputConfig.myExtension = '.csv'
+def exampleUserPlugin(dd4hepSimulation):
+     from DDG4 import EventAction, Kernel
+     dd = dd4hepSimulation  
+     evt_root = EventAction(Kernel(), 'SCEGeant4Output2ROOT/' + dd.outputFile, True)
+     evt_root.HandleMCTruth = True 
+     evt_root.Control = True
+     if not dd.outputFile.endswith(dd.outputConfig.myExtension):
+          output = dd.outputFile + dd.outputConfig.myExtension
+          evt_root.Output = output
+          evt_root.enableUI()
+          Kernel().eventAction().add(evt_root)
+     return None
+SIM.outputConfig.userOutputPlugin = exampleUserPlugin
+SIM.outputConfig.myExtension = '.root'
 ##     
-SIM.outputConfig.userOutputPlugin = None
+##SIM.outputConfig.userOutputPlugin = None
 
 
 ################################################################################
